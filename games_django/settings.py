beeeaf8ffcab7d7364e4f393ac75da40d6b4ea1f
Kirill-Korzunov <kirillkorzunov@gmail.com>
2023-05-23 14:45:13 +0300

    add start navigation

--- games_django/settings.py
+++ games_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'games.apps.GamesConfig',
+    'main.apps.MainConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'games_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'main/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'main/static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 # Create your views here.
+menu = [
+    {'title': 'Главная', 'url_name': 'home' },
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+]
+
+def index(request):
+    title = 'Главная страница'
+    content = 'Главная страница'
+    context = {
+        'title': title,
+        'menu': menu,
+        'content': content,
+    }
+    return render(request, 'base.html', context)
+
+def about(request):
+    title = 'О сайте'
+    content = 'О сайте'
+    context = {
+        'title': title,
+        'menu': menu,
+        'content': content,
+    }
+    return render(request, 'base.html', context)
+
+def contact(request):
+    title = 'Обратная связь'
+    content = 'Обратная связь'
+    context = {
+        'title': title,
+        'menu': menu,
+        'content': content,
+    }
+    return render(request, 'base.html', context)
+
+def login(request):
+    title = 'Войти'
+    content = 'ФОРМА ВХОДА'
+    context = {
+        'title': title,
+        'menu': menu,
+        'content': content,
+    }
+    return render(request, 'base.html', context)
+
+def register(request):
+    title = 'Регистрация'
+    content = 'ФОРМА РЕГИСТРАЦИИ'
+    context = {
+        'title': title,
+        'menu': menu,
+        'content': content,
+    }
+    return render(request, 'base.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
