be8ff97fcacd4a100b717c6b01a6eb86cb537753
Kirill-Korzunov <kirillkorzunov@gmail.com>
2023-05-23 14:51:41 +0300

    relation

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + ' ' + self.last_name
